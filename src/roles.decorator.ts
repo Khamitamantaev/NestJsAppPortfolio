95500fd31eeea04831a4f44ab066edf934e6f4b8
Khamit Amantaev <khamitamantaev@gmail.com>
2021-04-18 22:33:40 +0500

    Сохраню декоратор и enum

---
+++ src/roles.decorator.ts
@@ -0,0 +1,7 @@
+// Этот декоратор позволяет указать, какие роли требуются для доступа к определенным ресурсам.
+
+import { SetMetadata } from '@nestjs/common';
+import { Role } from './enums/role.enum';
+
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
