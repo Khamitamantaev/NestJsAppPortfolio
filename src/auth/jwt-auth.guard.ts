5ed0c5cb111a99e471a1968579d7d959cf15b3e8
Khamit Amantaev <khamitamantaev@gmail.com>
2021-04-18 21:35:47 +0500

    Update jwt-auth.guard.ts

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-
+import {
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    //можно добавлять логику аутентификации здесь
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info) {
+    //вывод ошибок в случае отсутствия юзера
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+
+}
